a4fc4442da0bd6d131792ff1222a83d2d21f5d95
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-16 02:13:29 +0300

    factrory & seeds build

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -2,7 +2,7 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function(Faker $faker){
+$factory->define(App\Article::class, function(Faker $faker) {
     return [
         'title' => $faker->text(50),
         'body' => $faker->text(200),
--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -11,6 +11,6 @@ class ArticlesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Articles::class, 30)->create();
+        factory(App\Article::class, 30)->create();
     }
 }
