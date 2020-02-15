bcd86ce2ace5110c974d463fe084f2d282b90e0c
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-16 02:51:07 +0300

    larticle build

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('articles', 'ArticleController@index');
+
+Route::get('article/{id}', 'ArticleController@show');
+
+Route::post('article', 'ArticleController@store');
+
+Route::put('articles', 'ArticleController@store');
+
+Route::delete('articles', 'ArticleController@destroy');
+
+
