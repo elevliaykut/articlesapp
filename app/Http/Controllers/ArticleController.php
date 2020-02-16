b27f86b49b239754cb001fd3b8621c7ccb96359b
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-16 03:08:16 +0300

    ArticleController Refactored

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Article;
+use App\Http\Resources\Article as ArticleResource;
+
 
 class ArticleController extends Controller
 {
@@ -14,7 +19,10 @@ class ArticleController extends Controller
     public function index()
     {
         // GET Article
-        
+        $articles = Article::paginate(15);
+
+        //Return collection of articles as a resource
+        return ArticleResource::collection($articles);
     }
 
     /**
