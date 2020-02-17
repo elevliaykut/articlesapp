316adaa5cc8bed0717783d4ad890f62de65c6721
elevliaykut <elevli.aykut.28@gmail.com>
2020-02-17 17:02:15 +0300

    Pagination bulding to fetch api

--- public/js/app.js
+++ public/js/app.js
@@ -1918,6 +1918,12 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   data: function data() {
     return {
@@ -1936,14 +1942,28 @@ __webpack_require__.r(__webpack_exports__);
     this.fetchArticles();
   },
   methods: {
-    fetchArticles: function fetchArticles() {
+    fetchArticles: function fetchArticles(page_url) {
       var _this = this;
 
-      fetch('api/articles').then(function (res) {
+      var vm = this;
+      page_url = page_url || 'api/articles';
+      fetch(page_url).then(function (res) {
         return res.json();
       }).then(function (res) {
         _this.articles = res.data;
+        vm.makePagination(res.meta, res.links);
+      })["catch"](function (err) {
+        return console.log(err);
       });
+    },
+    makePagination: function makePagination(meta, links) {
+      var pagination = {
+        current_page: meta.current_page,
+        last_page: meta.last_page,
+        next_page_url: links.next,
+        prev_page_url: links.pev
+      };
+      this.pagination = pagination;
     }
   }
 });
@@ -32755,18 +32775,62 @@ var render = function() {
     [
       _c("h2", [_vm._v("Articles")]),
       _vm._v(" "),
-      _vm._l(_vm.articles, function(article) {
-        return _c("div", { key: article.id, staticClass: "card card-body" }, [
-          _c("h3", [_vm._v(_vm._s(article.title))]),
+      _c("nav", { attrs: { "aria-label": "Page navigation example" } }, [
+        _c("ul", { staticClass: "pagination" }, [
+          _c(
+            "li",
+            {
+              staticClass: "page-item",
+              class: [{ disabled: !_vm.pagination.prev_page_url }]
+            },
+            [
+              _c(
+                "a",
+                {
+                  staticClass: "page-link",
+                  attrs: { href: "#" },
+                  on: {
+                    click: function($event) {
+                      return _vm.fetchArticles(_vm.pagination.prev_page_url)
+                    }
+                  }
+                },
+                [_vm._v("Previous")]
+              )
+            ]
+          ),
           _vm._v(" "),
-          _c("p", [_vm._v(_vm._s(article.body))])
+          _vm._m(0)
         ])
+      ]),
+      _vm._v(" "),
+      _vm._l(_vm.articles, function(article) {
+        return _c(
+          "div",
+          { key: article.id, staticClass: "card card-body mb-2" },
+          [
+            _c("h3", [_vm._v(_vm._s(article.title))]),
+            _vm._v(" "),
+            _c("p", [_vm._v(_vm._s(article.body))])
+          ]
+        )
       })
     ],
     2
   )
 }
-var staticRenderFns = []
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("li", { staticClass: "page-item" }, [
+      _c("a", { staticClass: "page-link", attrs: { href: "#" } }, [
+        _vm._v("Next")
+      ])
+    ])
+  }
+]
 render._withStripped = true
 
 
--- resources/assets/js/components/Articles.vue
+++ resources/assets/js/components/Articles.vue
@@ -1,7 +1,13 @@
 <template>
     <div>
         <h2>Articles</h2>
-        <div class="card card-body" v-for="article in articles" v-bind:key="article.id">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li v-bind:class="[{disabled: !pagination.prev_page_url}]" class="page-item"><a class="page-link" href="#" @click="fetchArticles(pagination.prev_page_url)">Previous</a></li>
+                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+        </nav>
+        <div class="card card-body mb-2" v-for="article in articles" v-bind:key="article.id">
             <h3>{{ article.title }}</h3>
             <p>{{ article.body }}</p>
         </div>
@@ -27,12 +33,25 @@
             this.fetchArticles();
         },
         methods:{
-            fetchArticles(){
-                fetch('api/articles')
+            fetchArticles(page_url){
+                let vm = this;
+                page_url=page_url || 'api/articles'
+                fetch(page_url)
                     .then(res=>res.json())
                     .then(res => {
                         this.articles = res.data;
+                        vm.makePagination(res.meta, res.links);
                     })
+                    .catch(err=>console.log(err));
+            },
+            makePagination(meta, links){
+                let pagination = {
+                    current_page: meta.current_page,
+                    last_page: meta.last_page,
+                    next_page_url: links.next,
+                    prev_page_url: links.pev,
+                }
+                this.pagination = pagination;
             }
         }
     }
